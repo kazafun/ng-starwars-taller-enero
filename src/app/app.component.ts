c53183b0247cb56ddd4eaa989f55b1c122bddd8f
ALEJANDRO JOSE FERNANDEZ TOLEDO <ajft1@clL27-6>
2017-01-27 20:43:02 +0100

    segunda clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http, Response} from "@angular/http";
+import "rxjs";
+import {SwapiService} from "./core/swapi.service";
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
+  people = [];
+
+  constructor(private swapiService:SwapiService){
+
+  }
+
+  ngOnInit(){
+    this.swapiService
+      .getPeopleList()
+      .subscribe((people) => this.people = people)
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SwapiService } from './core/swapi.service';
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [SwapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
